27589058bb12026106f9fcca3fc662cde55a090e
Jonas-Petrikas <jpetrik15@students.lcc.lt>
2024-11-26 17:08:49 +0200

    menu changes on scroll and scroll-back

--- app.js
+++ app.js
@@ -7,31 +7,49 @@ const NavItemTxt = document.querySelectorAll('.header-bin ul li a');
 const NavButton = document.querySelector ('.header-bin button.right');
 
 
-window.addEventListener('scroll', _ =>{
-    landing.style.backgroundColor = 'var(--brand-green)';
-    logoA.style.color = 'var(--white)';
-    logoASpan.style.color = 'var(--white)';
-    for(i = 0; i < NavItemTxt.length; i++){
-        NavItemTxt[i].style.color = 'var(--white)';
-    }
-    NavButton.style.color = 'var(--white)';
-    NavButton.style.backgroundColor = 'var(--brand-green)';
-    NavButton.style.border = '1px solid var(--white)';
 
+console.log (landing.dataset.statusas);
+console.log (window, window.scrollY);
+
+let statusas = landing.dataset.statusas;
+statusas = 'normal';
+
+// Add an event listener for the scroll event
+window.addEventListener('scroll', _ => {
+  console.log(statusas);
+  
+  if (window.scrollY > 50 ) {
+    // Add the 'header-scrolled' class when scrolling
+    statusas = 'scrolled';
+  } else {
+    // Remove the 'header-scrolled' class when back at the top
+    statusas = 'normal';
+  }
 });
 
-// // Select the header
-// const header = document.getElementById('header');
- 
-// // Add an event listener for the scroll event
-// window.addEventListener('scroll', () => {
-//   if (window.scrollY > 50) {
-//     // Add the 'header-scrolled' class when scrolling
-//     header.classList.add('header-scrolled');
-//     header.classList.remove('header-normal');
-//   } else {
-//     // Remove the 'header-scrolled' class when back at the top
-//     header.classList.remove('header-scrolled');
-//     header.classList.add('header-normal');
-//   }
-// });
+
+window.addEventListener('scroll', _ =>{
+    if (statusas === 'scrolled'){
+        landing.style.backgroundColor = 'var(--brand-green)';
+        logoA.style.color = 'var(--white)';
+        logoASpan.style.color = 'var(--white)';
+        for(i = 0; i < NavItemTxt.length; i++){
+            NavItemTxt[i].style.color = 'var(--white)';
+        }
+        NavButton.style.color = 'var(--white)';
+        NavButton.style.backgroundColor = 'var(--brand-green)';
+        NavButton.style.border = '1px solid var(--white)';
+    } else {
+        landing.style.backgroundColor = 'var(--brand-green-light)';
+        logoA.style.color = 'black';
+        logoASpan.style.color = 'var(--brand-green)';
+        for(i = 0; i < NavItemTxt.length; i++){
+            NavItemTxt[i].style.color = 'var(--brand-green)';
+        }
+        NavButton.style.color = 'var(--brand-green)';
+        NavButton.style.backgroundColor = 'var(--brand-green-light)';
+        NavButton.style.border = '1px solid var(--brand-green)';
+    }
+});
+
+
